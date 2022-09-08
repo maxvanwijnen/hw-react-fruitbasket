5e4d29ae5aef3d9b7028e6025f6c0b65d385e5ac
maxvanwijnen <maxvanwijnen@gmail.com>
2022-09-08 21:36:23 +0200

    bestel pagina

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import Basket from "./components/Basket.js";
+import OrderForm from "./components/OrderForm";
 
 function App() {
+
+
+
+
   return (
     <>
       <h1>Fruitmand bezorgservice</h1>
+        <Basket />
+        <OrderForm />
     </>
   );
 }
